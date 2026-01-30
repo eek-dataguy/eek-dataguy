eef4251c697a24e79cffa771839276110d442284
E Ek <eek.datasc@gmail.com>
2026-01-30 10:15:22 +0000

    initial version

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
+  basePath: "/eek-dataguy",
+  assetPrefix: "/eek-dataguy/",
 };
 
 export default nextConfig;
